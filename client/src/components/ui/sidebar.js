f1ee09b5bc0029472b17fe10717ddbd59a2f0393
im161726 <mathieu.ibersien@etu.univ-lehavre.fr>
2020-07-19 18:27:03 +0200

    ajout de contact me dans la side bar et un peu de css

--- client/src/components/ui/sidebar.js
+++ client/src/components/ui/sidebar.js
@@ -27,28 +27,50 @@ class SideBar extends Component {
 
         return (
 
-            <div className={this.state.sidebarStyle}> {/*revoir l'exemple dans le projet redux*/}
+            <div className={this.state.sidebarStyle}> {/* TODO revoir l'exemple dans le projet redux*/}
 
                 <button className={"openbtn"} onClick={() => this.closeHandler()}>Close</button>
 
-                <ul className={"ul"}>
-                    <Link to="/">
-                        <li>Welcome</li>
-                    </Link>
-                    <Link to="/movies">
-                        <li>Movies</li>
-                    </Link>
-                    <Link to="/customers">
-                        <li>Customers</li>
-                    </Link>
-                    <Link to="/users">
-                        <li>Users</li>
-                    </Link>
-                    <Link to="/books">
-                        <li>Books</li>
-                    </Link>
-                    
-                </ul>
+                <div>
+                    <ul className={"ul"}>
+                        <Link to="/">
+                            <li>Welcome</li>
+                        </Link>
+                        <Link to="/movies">
+                            <li>Movies</li>
+                        </Link>
+                        <Link to="/customers">
+                            <li>Customers</li>
+                        </Link>
+                        <Link to="/users">
+                            <li>Users</li>
+                        </Link>
+                        <Link to="/books">
+                            <li>Books</li>
+                        </Link>
+                    </ul>
+                </div>
+   
+                <div className="contact">
+                    <h4>Contact me: </h4>
+                    <ul>
+                        <li>
+                            <a href="https://www.facebook.com/mathieu.ibersien/">Facebook</a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/Maaaaamath">Twitter</a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/mamath_ib/">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/">LinkedIn comming out soon...</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/">GitHub comming out soon...</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
