8d12e5735831994a131e86a70cbb276499ff1971
Mathieu Ibersien <mat98-76@live.fr>
2021-07-08 22:26:32 +0200

    Up des variables d'environnement pour databases: projet mort..

--- Database/database.js
+++ Database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const assert = require('assert');
 require('dotenv').config();
 
-const url = process.env.MONGO_DB_CLUSTER0_PSW_ADMIN || process.env.MONGO_DB_CLUSTER0_PSW;
+const url = process.env.DATABASE_URL_CLUSTER_ADMIN || process.env.DATABASE_URL_CLUSTER_READ_ONLY;
 
 // Connect to mongo
 let db = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true },  
